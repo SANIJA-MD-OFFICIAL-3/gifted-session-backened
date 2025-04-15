ce38f781ceae7158334006d6d5c8b16683ee5032
Maurice Gift <ceo@giftedtech.web.id>
2025-04-15 13:34:57 +0300

    Gifted

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 // const bcrypt = require('bcryptjs');
-const Creds = require('./credsModel');
+const Creds = require('./models/credsModel');
 const path = require('path');
 
 const app = express();
